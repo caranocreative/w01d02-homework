0879a79fe1e5947755921327727d9c9052b2cc55
Laurie Carano <lauriecarano@gmail.com>
2018-06-12 23:06:13 -0600

    Commit 9 - Created loops.js file

---
+++ loops.js
@@ -0,0 +1,30 @@
+for (i = 0; i <= 99; i++) {
+ 	console.log(i);
+ }
+
+for (i = 99; i >= 0; i--) {
+ 	console.log(i);
+ }
+
+for (i = 2; i < 98; i+=2) {
+   console.log(i);
+  }
+
+for (i = 98; i > 2; i-=2) {
+ 	console.log(i);
+ }
+
+console.log("------- 49 to 72");
+for (i = 49; i <= 72; i++) {
+	console.log(i);
+}
+
+console.log("------- 81 to 26");
+for (i = 81; i >= 26; i--) {
+	console.log(i);
+}
+
+console.log("------- 3 to 90");
+for (i = 3; i <= 90; i += 3) {
+	console.log(i);
+}
